72393181299943b5de752927f736d4bb9b26848b
Viet Hung <viet.hung.2898@gmail.com>
2023-02-20 17:37:44 +0700

    add

---
+++ routes/default.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('clear-cache', function () {
+  Artisan::call('config:clear');
+  Artisan::call('view:clear');
+  Artisan::call('cache:clear');
+//    Artisan::call('cms:publish:assets');
+//    Artisan::call('storage:link');
+  return 'xong';
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::group(['prefix' => 'admin'], function () {
+  Voyager::routes();
+});
